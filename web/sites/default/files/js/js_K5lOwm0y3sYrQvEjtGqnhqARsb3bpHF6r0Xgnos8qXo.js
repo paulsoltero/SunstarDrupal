f2757ad9b4bb49d20846fcc4ff288bd46482b8f3
Paul Angelo Soltero <paulangelo.soltero@gmail.com>
2023-03-08 14:27:50 +0800

    Replicate About Us Page

---
+++ web/sites/default/files/js/js_K5lOwm0y3sYrQvEjtGqnhqARsb3bpHF6r0Xgnos8qXo.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bootstrap_subtheme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
+;
